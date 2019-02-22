c5528001d1791ce317ccd2573b2faa4a4575c2e6
jozimarback <jozimar.back@evosix.com.br>
2019-02-21 23:19:10 -0300

    created first query on schema

--- src/index.js
+++ src/index.js
@@ -1,1 +1,25 @@
-console.log('hello graphql')
+import {GraphQLServer} from 'graphql-yoga'
+
+const typeDefs = `
+    type Query {
+        hello:String!
+    }
+`
+
+const resolvers={
+    Query:{
+        hello(){
+            return 'This is my fisrst query!'
+        }
+    }
+}
+
+
+const server= new GraphQLServer({
+    typeDefs,
+    resolvers
+})
+
+server.start(() => {
+    console.log('The server is up!')
+})
