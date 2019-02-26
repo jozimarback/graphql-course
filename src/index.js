87c6e078187e9cd4e5495007a671e8fbfaeb1fb2
jozimarback <jozimar.back@evosix.com.br>
2019-02-25 22:14:54 -0300

    created user and post type

--- src/index.js
+++ src/index.js
@@ -1,37 +1,48 @@
-import {GraphQLServer} from 'graphql-yoga'
+import { GraphQLServer } from 'graphql-yoga'
 
 const typeDefs = `
     type Query {
+        me: User!
+    }
+
+    type User {
         id:ID!
         name:String!
-        age:Int!
-        employed:Boolean!
-        gpa:Float
+        email:String!
+        age:Int
+    }
+
+    type Post {
+        id:ID!
+        title:String!
+        body:String!
+        published:Boolean!
     }
 `
 
-const resolvers={
-    Query:{
-        id(){
-            return 'abc123'
+const resolvers = {
+    Query: {
+        me() {
+            return {
+                id: '123099',
+                name: 'Jozimar',
+                email: 'jozimar@email.com',
+                age: 29
+            }
         },
-        name(){
-            return 'Jozimar'
-        },
-        age(){
-            return 30
-        },
-        employed(){
-            return true
-        },
-        gpa(){
-            return null
+        post() {
+            return {
+                id: '092',
+                title: 'GraphQL 101',
+                body:'',
+                published:false
+            }
         }
     }
 }
 
 
-const server= new GraphQLServer({
+const server = new GraphQLServer({
     typeDefs,
     resolvers
 })
