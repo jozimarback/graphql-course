566b0cff6436917f59fe39acd70986fdb0e9e23d
Jozimar Back <jozimar5@gmail.com>
2019-03-10 14:18:23 -0300

    add spread operator to babel and code js

--- src/index.js
+++ src/index.js
@@ -144,9 +144,7 @@ const resolvers = {
             
             const user = {
                 id:uuidv4(),
-                name:args.name,
-                email:args.email,
-                age:args.age
+                ...args
             }
             users.push(user);
             return user;
@@ -157,10 +155,7 @@ const resolvers = {
                 throw new Error('User not found')
             const post = {
                 id:uuidv4(),
-                title:args.title,
-                body:args.body,
-                published:args.published,
-                author:args.author
+                ...args
             }
 
             posts.push(post);
@@ -176,9 +171,7 @@ const resolvers = {
 
             const comment = {
                 id:uuidv4(),
-                text:args.text,
-                author:args.author,
-                post:args.post
+                ...args
             };
             comments.push(comment);
             return comment;
