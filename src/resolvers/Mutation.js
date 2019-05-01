603aab12af566d753808802d6de962916d7efe84
Jozimar Back <jozimar5@gmail.com>
2019-05-01 20:12:49 -0300

    mutation subscribe post

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -73,9 +73,10 @@ const mutation = {
         }
         return post;
     },
-    updatePost(parent, args, { db }, info) {
+    updatePost(parent, args, { db, pubsub }, info) {
         const { id, data } = args;
-        const post = db.post.find((post) => post.id === id)
+        const post = db.post.find((post) => post.id === id);
+        const originalPost = { ...post };
         if (!post) {
             throw new Error('Post not found')
         }
@@ -87,17 +88,47 @@ const mutation = {
         }
         if (typeof data.published == 'boolean') {
             post.published = data.published;
+            if (originalPost.published && !post.published) {
+                pubsub.publish('post', {
+                    post: {
+                        mutation: 'DELETED',
+                        data: originalPost
+                    }
+                })
+            } else if (!originalPost.published && post.published) {
+                pubsub.publish('post', {
+                    post: {
+                        mutation: 'CREATED',
+                        data: post
+                    }
+                })
+            }
+        } else if (post.published) {
+            pubsub.publish('post', {
+                post: {
+                    mutation: 'UPDATED',
+                    data: post
+                }
+            })
         }
 
         return post;
     },
-    deletePost(parent, args, { db }, info) {
+    deletePost(parent, args, { db, pubsub }, info) {
         const postIndex = db.posts.findIndex(post => post.id === args.id)
         if (postIndex === -1)
             throw new Error('Post not found')
-        const deletedPosts = db.posts.splice(postIndex, 1);
+        const [post] = db.posts.splice(postIndex, 1);
         comments = db.comments.filter(comment => comment.post !== args.id);
-        return deletedPosts[0];
+        if (post.published) {
+            pubsub.publish('post', {
+                post: {
+                    mutation: 'DELETED',
+                    data: post
+                }
+            })
+        }
+        return post;
     },
     createComment(parent, args, { db, pubsub }, info) {
         const userExistis = db.users.some(u => u.id === args.data.author);
